255e31c8b4214566edaa292082ee52e96c640f56
httien-1001 <tien.ht187644@sis.hust.edu.vn>
2020-04-14 00:53:41 +0700

    12:53

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,5 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(0, 100);
-ctx.stroke();
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// ctx.moveTo(0, 0);
+// ctx.lineTo(0, 100);
+// ctx.stroke();
